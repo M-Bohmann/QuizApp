45ccec4b7de75d1e84512eb7ed99f9a7e612ab74
Marcus Bohmann <mbohmann@gmx.de>
2023-05-03 16:23:13 +0200

    add save bookmarks

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ const bodyElement = document.querySelector('[data-js="body"]');
 darkModeToggle.addEventListener("click", () => {
   bodyElement.classList.toggle("dark");
 });
+
+cards = document.querySelectorAll('[data-js="quiz-cards""]');
+cards.forEach((card) => {
+  const saveBookmark = document.querySelector('[data-js="save-bookmark"]');
+  saveBookmark.addEventListener("click", () => {
+    saveBookmark.classList.toggle("bookmark");
+  });
+});
